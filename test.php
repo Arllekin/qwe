00f6888086a8bb5ef58b04ce8f14229f4dc2e961
Vlad <ege>
2021-09-04 22:10:39 +0300

    It_Works2

--- test.php
+++ test.php
@@ -54,27 +54,21 @@ class Color
 
 //  Mix_Function_Begin
             public function mix (Color $second): Color {
-               return new Color(($this -> getRed() + $second ->getRed())/2,
-                                ($this->getGreen() + $second ->getGreen()) /2,
-                                ($this ->getBlue() + $second ->getBlue()) /2
+               return new Color(intdiv($this -> getRed() + $second ->getRed(),'2'),
+                                intdiv($this->getGreen() + $second ->getGreen(),'2'),
+                                intdiv($this ->getBlue() + $second ->getBlue(),'2')
                );
             }
 //  Mix_Function_End
 
+//  Equals_Function_Begin
             public function equals (Color $second):bool  {
-               $r = null;
-               $g = null;
-               $b = null;
-               $result = null;
 
-               if ($this->getRed() == $second->getRed())        {$r = true;} else{$r = false;}
-               if ($this->getGreen() == $second->getGreen())    {$g = true;} else{$g = false;}
-               if ($this ->getBlue() == $second->getBlue())     {$b = true;} else{$b = false;}
-
-               if ($r == true & $g == true & $b == true)        {$result = true;} else{$result = false;}
-
-               return $result;
+                return $this->getRed() == $second->getRed() &&
+                       $this->getGreen() == $second->getGreen() &&
+                       $this ->getBlue() == $second->getBlue();
             }
+//  Equals_Function_End
 
     };
 
